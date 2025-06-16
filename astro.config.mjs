6a15b9af1e8585542198904bbabb5ca1b5e69c5c
nikosetiawanp <nikosetiawanp@gmail.com>
2025-06-16 13:23:48 +0700

    setup tailwindcss theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://nikosetiawanp.github.io',
     base: 'frontendmentor-personal-blog',
+    vite: {
+    plugins: [tailwindcss()],
+  },
 });
