86e5b55d610cb0e051d9e8bcdf1eedbe1857bb4a
nikosetiawanp <nikosetiawanp@gmail.com>
2025-06-21 11:22:06 +0700

    style markdown pages

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,16 +1,16 @@
 // Import the glob loader
-import { glob } from "astro/loaders";
+// import { glob } from "astro/loaders";
 // Import utilities from `astro:content`
 import { z, defineCollection } from "astro:content";
 // Define a `loader` and `schema` for each collection
 const blog = defineCollection({
-    loader: glob({ pattern: '**/[^_]*.md', base: "./src/blog" }),
+    type: 'content',
     schema: z.object({
       title: z.string(),
-      slug : z.string(),
-      publishedAt: z.date(),
+      slug : z.string().optional(),
+      publishedAt: z.string(),
       description: z.string(),
-      tags: z.array(z.string())
+      tags: z.array(z.string()).optional()
     })
 });
 // Export a single `collections` object to register your collection(s)
