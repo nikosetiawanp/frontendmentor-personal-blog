088da0b67b52e0e5548570766ad1423b85a4c665
nikosetiawanp <nikosetiawanp@gmail.com>
2025-06-25 19:59:28 +0700

    remove newsletter form validation functionality, fix deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,10 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import customTheme from './src/scripts/custom-theme';
-import { rehypeShiki } from '@astrojs/markdown-remark';
 
 // https://astro.build/config
 export default defineConfig({
+    output: 'static',
     site: 'https://nikosetiawanp.github.io',
     base: 'frontendmentor-personal-blog',
     vite: {
